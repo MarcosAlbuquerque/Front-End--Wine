c85f482d7d6f829964f37782614364806132d366
Marcos Albuquerque <desenvolvedorwebfrontend@gmail.com>
2022-06-12 00:24:36 -0300

    feat: Adição de fonte no head, criação do corpo da página

--- winedesk/pages/index.tsx
+++ winedesk/pages/index.tsx
@@ -1,11 +1,13 @@
 import type { NextPage } from 'next';
 import Head from 'next/head';
 import { createGlobalStyle } from 'styled-components';
+import Body from '../components/Body';
 import Header from '../components/Header';
 
-const StyledGlobal = createGlobalStyle`
+export const StyledGlobal = createGlobalStyle`
   body {
     margin: 0;
+    background-color: #F5F5F5;
   }
 `;
 
@@ -17,9 +19,11 @@ const Home: NextPage = () => {
         <meta name='description' content='Desafio tecnico Wine' />
         <link rel='icon' href='/favicon.ico' />
         <link href="http://fonts.cdnfonts.com/css/neo-sans-std-tr?styles=27758" rel="stylesheet" />
+        <link href="http://fonts.cdnfonts.com/css/lato?styles=17388,17381" rel="stylesheet" />
       </Head>
       <StyledGlobal />
       <Header />
+      <Body />
     </>
   );
 };
